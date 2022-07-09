c33e01cabc2907d91fb12a1389b80de55cd5f733
Muhammad Saad Mateen <saad.5690@gmail.com>
2022-07-09 16:07:58 +0500

    Completed 3rd Chapter

--- Ch03/03_05/ctype.go
+++ Ch03/03_05/ctype.go
@@ -3,6 +3,8 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 )
 
 func main() {
@@ -17,6 +19,14 @@ func main() {
 // contentType will return the value of Content-Type header returned by making an
 // HTTP GET request to url
 func contentType(url string) (string, error) {
-	// FIXME
-	return "", nil
+	resp, err := http.Get(url)
+	if err != nil {
+		return "Incorrect Error or Site is Down!", err
+	}
+	defer resp.Body.Close()
+	body, err2 := io.ReadAll(resp.Body)
+	if err2 != nil {
+		return "Some Error", err2
+	}
+	return http.DetectContentType(body), nil
 }
